0cb18315819303357492f0523a30fcb2889d13aa
niloufar60 <40465469+niloufar60@users.noreply.github.com>
2018-09-06 13:50:40 -0400

    Add files via upload

---
+++ func_exception_proj1.py
@@ -0,0 +1,11 @@
+import pandas as pd
+tweet_df = pd.read_csv("tweets.csv")
+# Select retweets from the Twitter DataFrame: result
+result = filter(lambda x: x[0:2]=='RT', tweets_df['text'])
+
+# Create list from filter object result: res_list
+res_list=list(result)
+
+# Print all retweets in res_list
+for tweet in res_list:
+    print(tweet)
